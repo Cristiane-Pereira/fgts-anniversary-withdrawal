fff9bee2e8fc3b21663302bfcb33f9a25e9b976b
Cristiane Silva <cristiane.pereira901113@gmail.com>
2025-03-25 09:53:52 -0300

    Ajuste de código

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,13 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-    async redirects() {
-      return [
-        {
-          source: '/',
-          destination: '/initial',
-          permanent: true,
-        },
-      ]
-    }
-  }
-  
-  export default nextConfig
-  
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/initial',
+        permanent: false,
+      },
+    ];
+  },
+};
+
+export default nextConfig;
